9d904b4219a03bbaba15d191e581fa2317c6ee46
BenchBeach <1779227906@qq.com>
2021-04-24 01:15:26 +0800

    feat

--- server/master.js
+++ server/master.js
@@ -1,3 +1,39 @@
 import os from 'os';
 import cp from 'child_process'
+import net from 'net'
+import {port,hostname} from './config.js'
+import readline from 'readline';
 
+const numCPUs=os.cpus().length;
+
+let worker = [];
+let socketCnt=0;
+
+for(let i=0;i<numCPUs;i++){
+    worker.push(cp.fork('server.js',[i]))
+}
+
+const server = new net.createServer({ pauseOnConnect: true });
+
+server.on('connection', (client)=>{
+    worker[socketCnt].send('socket',client,{keepOpen:ture});
+})
+
+server.listen(port, hostname, () => {
+    console.log(`服务器运行在：http://${hostname}:${port}`);
+    (function printLogo() {
+        console.log("     ______   __    __  __        ______  __    __  ________         ______   __    __   ______   ________ \n    /      \\ /  \\  /  |/  |      /      |/  \\  /  |/        |       /      \\ /  |  /  | /      \\ /        |\n   /$$$$$$  |$$  \\ $$ |$$ |      $$$$$$/ $$  \\ $$ |$$$$$$$$/       /$$$$$$  |$$ |  $$ |/$$$$$$  |$$$$$$$$/ \n   $$ |  $$ |$$$  \\$$ |$$ |        $$ |  $$$  \\$$ |$$ |__          $$ |  $$/ $$ |__$$ |$$ |__$$ |   $$ |   \n   $$ |  $$ |$$$$  $$ |$$ |        $$ |  $$$$  $$ |$$    |         $$ |      $$    $$ |$$    $$ |   $$ |   \n   $$ |  $$ |$$ $$ $$ |$$ |        $$ |  $$ $$ $$ |$$$$$/          $$ |   __ $$$$$$$$ |$$$$$$$$ |   $$ |   \n   $$ \\__$$ |$$ |$$$$ |$$ |_____  _$$ |_ $$ |$$$$ |$$ |_____       $$ \\__/  |$$ |  $$ |$$ |  $$ |   $$ |   \n   $$    $$/ $$ | $$$ |$$       |/ $$   |$$ | $$$ |$$       |      $$    $$/ $$ |  $$ |$$ |  $$ |   $$ |   \n    $$$$$$/  $$/   $$/ $$$$$$$$/ $$$$$$/ $$/   $$/ $$$$$$$$/        $$$$$$/  $$/   $$/ $$/   $$/    $$/    ")
+    })()
+});
+
+// cli module
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (msg) => {
+    if(msg=='q'){
+        process.exit();
+    }
+})
--- server/server.js
+++ server/server.js
@@ -2,48 +2,50 @@ import net from 'net';
 import readline from 'readline';
 import { v1 as uuid } from 'uuid';
 import controller from './controller.js';
-import {port,hostname} from './config.js'
 
 let sessions = {};
 
-const server = new net.createServer();
+process.on('message', (m, client) => {
+    if (m === 'socket') {
+        if (client) {
+            client.id = uuid();
+            sessions[client.id] = new controller(client);
+            sessions[client.id].getSessions(sessions)
+            client.on('data', data => {
+                //console.log(`客户端${client.name}发来一个信息：${data}`);
+                sessions[client.id].handlePkg(data);
+            });
 
-server.on('connection', (client) => {
-    client.id = uuid();
-    sessions[client.id] =new controller(client);
-    sessions[client.id].getSessions(sessions)
-    client.on('data', data => {
-        //console.log(`客户端${client.name}发来一个信息：${data}`);
-        sessions[client.id].handlePkg(data);
-    });
-
-    client.on('error', e => {
-        console.log('client error:' + e);
-        client.end();
-        delete sessions[client.id];
-    });
-    client.on('close', () => {
-        delete sessions[client.id];
-        console.log(`客户端${client.name}下线了`);
-    });
-
-});
-
-server.listen(port, hostname, () => {
-    console.log(`服务器运行在：http://${hostname}:${port}`);
-    (function printLogo() {
-        console.log("     ______   __    __  __        ______  __    __  ________         ______   __    __   ______   ________ \n    /      \\ /  \\  /  |/  |      /      |/  \\  /  |/        |       /      \\ /  |  /  | /      \\ /        |\n   /$$$$$$  |$$  \\ $$ |$$ |      $$$$$$/ $$  \\ $$ |$$$$$$$$/       /$$$$$$  |$$ |  $$ |/$$$$$$  |$$$$$$$$/ \n   $$ |  $$ |$$$  \\$$ |$$ |        $$ |  $$$  \\$$ |$$ |__          $$ |  $$/ $$ |__$$ |$$ |__$$ |   $$ |   \n   $$ |  $$ |$$$$  $$ |$$ |        $$ |  $$$$  $$ |$$    |         $$ |      $$    $$ |$$    $$ |   $$ |   \n   $$ |  $$ |$$ $$ $$ |$$ |        $$ |  $$ $$ $$ |$$$$$/          $$ |   __ $$$$$$$$ |$$$$$$$$ |   $$ |   \n   $$ \\__$$ |$$ |$$$$ |$$ |_____  _$$ |_ $$ |$$$$ |$$ |_____       $$ \\__/  |$$ |  $$ |$$ |  $$ |   $$ |   \n   $$    $$/ $$ | $$$ |$$       |/ $$   |$$ | $$$ |$$       |      $$    $$/ $$ |  $$ |$$ |  $$ |   $$ |   \n    $$$$$$/  $$/   $$/ $$$$$$$$/ $$$$$$/ $$/   $$/ $$$$$$$$/        $$$$$$/  $$/   $$/ $$/   $$/    $$/    ")
-    })()
-});
-
-
-let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.on('line', (msg) => {
-    if(msg=='q'){
-        process.exit();
+            client.on('error', e => {
+                console.log('client error:' + e);
+                client.end();
+                delete sessions[client.id];
+            });
+            client.on('close', () => {
+                delete sessions[client.id];
+                console.log(`客户端${client.name}下线了`);
+            });
+        }
     }
-})
+});
+// server.on('connection', (client) => {
+//     client.id = uuid();
+//     sessions[client.id] =new controller(client);
+//     sessions[client.id].getSessions(sessions)
+//     client.on('data', data => {
+//         //console.log(`客户端${client.name}发来一个信息：${data}`);
+//         sessions[client.id].handlePkg(data);
+//     });
+
+//     client.on('error', e => {
+//         console.log('client error:' + e);
+//         client.end();
+//         delete sessions[client.id];
+//     });
+//     client.on('close', () => {
+//         delete sessions[client.id];
+//         console.log(`客户端${client.name}下线了`);
+//     });
+
+// });
+
